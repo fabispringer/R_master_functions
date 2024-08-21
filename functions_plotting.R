74fa3ee065c8e73b15cd3d35b330c1d26f5401cd
Fabian Springer <fspringe@login01.cluster.embl.de>
2024-08-21 11:21:00 +0200

    added diagonally split heatmap tiles

--- functions_plotting.R
+++ functions_plotting.R
@@ -666,5 +666,93 @@ cluster_rows = F,cluster_columns =F) {
 }
 
 
+#*  A diagonally split version of GeomTile ----
+# by [jan-glx](https://github.com/jan-glx), 
+# based on ggplot2::GeomTile by the [ggplot2 authors](https://github.com/tidyverse/ggplot2/graphs/contributors)
+draw_key_split_tile <- function(data, params, size) {
+  
+  data$width <- data$width %||% params$width %||% 1
+  data$height <- data$height %||% params$height %||% 1
+  data$frac<- data$frac %||% params$frac %||% 0.5
+  data$width[is.na(data$width)] <- 1
+  data$height[is.na(data$height)] <- 1
+  data$frac[is.na(data$frac)] <- 0.5
+  if (isTRUE(data$split)) {
+    x <- c(0, 1, 0)
+    y <- c(0, 1, 1)
+  } else {
+    x <- c(0, 1, 1)
+    y <- c(0, 1, 0)
+  }
+  x <- 0.5 + (x-0.5) * data$width
+  y <- 0.5 + (y-0.5) * data$height
+  
+  grid::polygonGrob(
+    x = x,
+    y = y,
+    default.units = "npc",
+    gp = grid::gpar(
+      col = data$colour,
+      fill = alpha(data$fill, data$alpha)
+    )
+  )
+}
+geom_split_tile <- function(mapping = NULL, data = NULL,
+                            stat = "identity", position = "identity",
+                            ...,
+                            linejoin = "mitre",
+                            na.rm = FALSE,
+                            show.legend = NA,
+                            inherit.aes = TRUE) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomSplitTile,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = rlang:::list2(
+      linejoin = linejoin,
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+GeomSplitTile <- ggproto(
+  "GeomSplitTile", GeomPolygon,
+  extra_params = c("na.rm"),
+  
+  setup_data = function(data, params) {
+    data$width <- data$width %||% params$width %||% resolution(data$x, FALSE)
+    data$height <- data$height %||% params$height %||% resolution(data$y, FALSE)
+    data$frac<- data$frac %||% params$frac %||% 0.5
+    data$split <- as.factor(data$split %||% params$split %||% FALSE)
+    
+    K = 5
+    n <- nrow(data)
+    z <- pmin(data$frac, 0.5)*2
+    zz <- pmin(1-data$frac, 0.5)*2
+    
+    new_data <- data.frame(
+      x = rep(data$x, each=K) + rep(3-as.integer(data$split)*2, each=K) * as.vector(rbind(-1,  1, 1, zz, -z)) * rep(data$width / 2, each=K),
+      y = rep(data$y, each=K) + rep(3-as.integer(data$split)*2, each=K) * as.vector(rbind(-1, -1, 1, z, -zz)) * rep(data$height / 2, each=K),
+      group = rep(seq_len(n), each=K)
+    )
+    new_data <- cbind(new_data, data[rep(seq_len(n), each = K), setdiff(colnames(data), c("x", "y", "group")), drop = FALSE])
+    new_data
+  },
+  
+  default_aes = aes(fill = "grey20", colour = NA, linewidth = 0.1, linetype = 1,
+                    alpha = NA, width = NA, height = NA, frac =0.5),
+  
+  required_aes = c("x", "y", "split"),
+  
+  draw_key = draw_key_split_tile
+)
+scale_split <- function(..., scale_name="scale_direction", palette = function(n) if(n>2) error(paste0(scale_name, " can handle at most 2 levels")) else c(FALSE, TRUE)) discrete_scale(aesthetics = "split", scale_name=scale_name, palette = palette, ... )
+
+
+
 
 
